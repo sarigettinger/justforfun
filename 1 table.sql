8a7f9762326da34b20999b7f49c08309a7ae6301
sarigettinger <sarigettinger@gmail.com>
2023-12-12 18:07:38 -0500

    gitignore

--- 1 table.sql
+++ 1 table.sql
@@ -2,3 +2,10 @@ create table dbo.body
 (id int not null identity primary key, 
 bodypart varchar(100) not null, 
 num int not null)
+
+insert body(bodypart, num)
+select 'eyes', 2
+union select 'ears', 2
+union select 'nose', 1
+union select 'mouth', 2
+union select 'teeth', 32
